3c33347c3a8abc6cbe053361481ca7a064feaaae
s53kr-lm8a6r <iam.msams@gmail.com>
2022-05-08 19:25:38 +0700

    spring starter

---
+++ src/test/java/id/msams/webrepo/WebrepoApplicationTests.java
@@ -0,0 +1,13 @@
+package id.msams.webrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebrepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
