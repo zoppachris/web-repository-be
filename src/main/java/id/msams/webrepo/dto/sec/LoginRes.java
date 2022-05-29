4583494b31dbb48b6cfc13faf24654996a67654d
s53kr-lm8a6r <iam.msams@gmail.com>
2022-05-29 21:02:22 +0700

    remove sch

--- src/main/java/id/msams/webrepo/dto/sec/LoginRes.java
+++ src/main/java/id/msams/webrepo/dto/sec/LoginRes.java
@@ -5,7 +5,6 @@ import java.util.Map;
 import com.toedter.spring.hateoas.jsonapi.JsonApiId;
 import com.toedter.spring.hateoas.jsonapi.JsonApiTypeForClass;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -21,11 +20,8 @@ public class LoginRes {
   @JsonApiId
   private Long id;
 
-  @Schema(example = "Bearer", description = "Type of credentials given for access. Current implementation only supports Bearer token.")
   private String type;
-  @Schema(example = "eyJ…iJ9.eyJ…OX0.FHX…5vg", description = "Access token credential to include within each request. A three-segmented token string delimited with period (.) symbol, implemented using JWT standard.")
   private String accessToken;
-  @Schema(ref = "#/components/schemas/jwt-claims")
   private Map<String, Object> claims;
 
 }
